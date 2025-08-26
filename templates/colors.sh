52f3c4f4995fe7badae2d232a62c84408e39a0ef
Luke Hogan <luke.hogan@hey.com>
2025-08-26 10:26:27 -0600

    Use color variables as placeholders in template files

---
+++ templates/colors.sh
@@ -0,0 +1,10 @@
+# Modify this with your color palette,
+# put in your new theme directory,
+# then run the generator script.
+
+COLOR_TEXT_PRIMARY='#<HEXCOLOR>'
+COLOR_TEXT_SECONDARY='#<HEXCOLOR>'
+COLOR_BACKGROUND='#<HEXCOLOR>'
+COLOR_HIGHLIGHT_PRIMARY='#<HEXCOLOR>'
+COLOR_HIGHLIGHT_SECONDARY='#<HEXCOLOR>'
+COLOR_ICON='#<HEXCOLOR>'
