133e2ac3a0141255229bc1f43badef6c59ad4551
Luke Hogan <luke.hogan@hey.com>
2025-08-29 09:41:09 -0600

    Add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DRESSCODE_PATH="${FCITX_DRESSCODE_PATH:-$HOME/.local/share/fcitx-dresscode}"
+FCITX_PATH="${FCITX_ROOT:-$HOME/.local/share/fcitx5}/themes"
+THEME_SETTER="$OMARCHY_PATH/bin/omarchy-theme-set"
+
+# Unlink the control scripts
+if [[ -f "$OMARCHY_PATH/bin/omarchy-fcitx-theme-set" ]]; then
+	rm "$OMARCHY_PATH/bin/omarchy-fcitx-theme-set"
+fi
+
+if [[ -f "$OMARCHY_PATH/bin/omarchy-restart-fcitx" ]]; then
+	rm "$OMARCHY_PATH/bin/omarchy-restart-fcitx"
+fi
+
+# Unpatch omarchy-theme-set
+cd "$OMARCHY_PATH"
+git checkout "bin/omarchy-theme-set"
+cd -
+
+# Unlink the theme files
+if [[ -d "$DRESSCODE_PATH/themes" ]]; then
+	cd "$DRESSCODE_PATH/themes"
+	for file in *; do
+		rm "$FCITX_PATH/$file"
+	done
+	cd -
+fi
+
+# Destroy the evidence
+rm -rf "$DRESSCODE_PATH"
+
+echo "Successfully uninstalled Fcitx Dress Code."
