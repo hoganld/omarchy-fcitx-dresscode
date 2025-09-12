f9c8e0b0e349f7f2a6cbe8cbee70f0ce74daa1ff
Luke Hogan <luke.hogan@hey.com>
2025-09-12 12:13:11 -0600

    create conf dir if not found

--- configure.sh
+++ configure.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-FCITX_CONF_DIR="$HOME/.config/fcitx5/conf"
+FCITX_BASE_DIR="$HOME/.config/fcitx5"
 
-if [[ ! -d "$FCITX_CONF_DIR" ]]; then
-  FCITX_CONF_DIR=$(gum input --prompt "Where is your Fcitx config directory? " --placeholder "/absolute/path/to/fcitx5/conf")
+if [[ ! -d "$FCITX_BASE_DIR" ]]; then
+  FCITX_BASE_DIR=$(gum input --prompt "Where is your Fcitx configuration directory? " --placeholder "$FCITX_BASE_DIR")
 fi
 
+FCITX_CONF_DIR="$FCITX_BASE_DIR/conf"
+
 if [[ ! -d "$FCITX_CONF_DIR" ]]; then
-  echo "Error: $FCITX_CONF_DIR not found."
-  exit 1
+  mkdir -p $FCITX_CONF_DIR
 fi
 
-FCITX_CONF_FILE="$FCITX_CONF_DIR/classicui.conf"
+FCITX_THEME_FILE="$FCITX_CONF_DIR/classicui.conf"
 
-if grep -qF "Theme=" "$FCITX_CONF_FILE" 2>/dev/null; then
-  sed -i "s/^Theme=.*$/Theme=current/" "$FCITX_CONF_FILE"
+if grep -qF "Theme=" "$FCITX_THEME_FILE" 2>/dev/null; then
+  sed -i "s/^Theme=.*$/Theme=current/" "$FCITX_THEME_FILE"
   echo "Updated Fcitx config to use the current Dress Code theme."
 else
-  echo "Theme=current" >>"$FCITX_CONF_FILE"
+  echo "Theme=current" >>"$FCITX_THEME_FILE"
   echo "Added Fcitx config to use the current Dress Code theme."
 fi
