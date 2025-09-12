27984ccde0f405b5b2d93ddf69eba94d3f4d14b1
Luke Hogan <luke.hogan@hey.com>
2025-09-12 13:06:20 -0600

    Save .prior-theme file to $DRESSCODE_PATH

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DRESSCODE_PATH="${FCITX_DRESSCODE_PATH:-$HOME/.local/share/fcitx-dresscode}"
 FCITX_BASE_DIR="$HOME/.config/fcitx5"
 
 if [[ ! -d "$FCITX_BASE_DIR" ]]; then
@@ -16,7 +17,7 @@ FCITX_THEME_FILE="$FCITX_CONF_DIR/classicui.conf"
 
 if grep -qF "Theme=" "$FCITX_THEME_FILE" 2>/dev/null; then
   PRIOR_THEME=$(grep "Theme=" $FCITX_THEME_FILE)
-  echo $PRIOR_THEME >.prior-theme
+  echo $PRIOR_THEME >$DRESSCODE_PATH/.prior-theme
   sed -i "s/^Theme=.*$/Theme=current/" "$FCITX_THEME_FILE"
   echo "Updated Fcitx config to use the current Dress Code theme."
 else
